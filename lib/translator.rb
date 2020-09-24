9971a8a0689ecf00a03327b61e7ae116b5841e6f
Milan Winter <milanwinter8@gmail.com>
2020-09-24 23:55:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,6 +14,7 @@ def get_japanese_emoticon(data,emoticon)
       return value[:japanese]
     end
   end
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(data,emoticon)
